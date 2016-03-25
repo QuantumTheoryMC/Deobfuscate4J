f98f5712b10009445f3b1b9f8a247069ccefb0e6
PizzaCrust <torchprojectserver@gmail.com>
2016-03-25 17:58:07 +0700

    applySpecialSource

---
+++ src/main/java/net/pizzacrust/deobfmc/ApplySpecialSource.java
@@ -0,0 +1,25 @@
+package net.pizzacrust.deobfmc;
+
+import net.md_5.specialsource.SpecialSource;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+
+public class ApplySpecialSource {
+    public static void main(String[] args) {
+        File srg = new File(SetupDirectories.deobfMcVersionFolder, "client.srg");
+        File tempJar = new File(SetupDirectories.deobfMcVersionFolder, "DeobfMC-1.9.jar");
+        File deobfMcJar = new File(SetupDirectories.deobfMcVersionFolder, "client.jar");
+        try {
+            FileUtils.copyFile(deobfMcJar, tempJar);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        String[] ssArgs = new String[] { "--quiet", "--srg-in", srg.getAbsolutePath(), "--in-jar", deobfMcJar.getAbsolutePath(), "--out-jar", tempJar.getAbsolutePath() };
+        try {
+            SpecialSource.main(ssArgs);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
--- src/main/java/net/pizzacrust/deobfmc/DownloadClient.java
+++ src/main/java/net/pizzacrust/deobfmc/DownloadClient.java
@@ -13,7 +13,7 @@ public class DownloadClient {
         try {
             String url = "https://launcher.mojang.com/mc/game/1.9/client/2f67dfe8953299440d1902f9124f0f2c3a2c940f/client.jar";
             URL urlObj = new URL(url);
-            File deobfMcJar = new File(SetupDirectories.deobfMcVersionFolder, "DeobfMC-1.9.jar");
+            File deobfMcJar = new File(SetupDirectories.deobfMcVersionFolder, "client.jar");
             FileUtils.copyURLToFile(urlObj, deobfMcJar);
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
